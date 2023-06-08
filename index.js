a268c5d93193dd86af9938c037c7bccdff84b599
srehan1 <syed.rehan@shoppertrak.com>
2023-06-08 12:55:25 +0530

    Adding ethers library and converting js files to modules

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
+import { ethers } from "https://cdn.ethers.io/lib/ethers-5.2.esm.min.js";
+
+// Identifiers
+const connectBtn = document.getElementById("connect-btn");
+const fundBtn = document.getElementById("fund-btn");
+
+console.log(ethers);
+
+// Functions
 async function connect() {
   if (typeof window.ethereum != "undefined") {
     await window.ethereum.request({ method: "eth_requestAccounts" });
     console.log("Metamask present!");
-    document.getElementById("connect-btn").innerHTML = "Connected to Metamask!";
+    connectBtn.innerHTML = "Connected to Metamask!";
   } else {
     console.log("Metamask is not installed.");
-    document.getElementById("connect-btn").innerHTML =
-      "Metamask is not installed.";
+    connectBtn.innerHTML = "Metamask is not installed.";
   }
 }
+
+async function fund(ethAmount) {
+  console.log(`Funding ${ethAmount} ETH.`);
+  if (typeof window.ethereum != "undefined") {
+    console.log("Adding funds.");
+  }
+}
+
+// Events
+connectBtn.onclick = connect;
+fundBtn.onclick = fund;
